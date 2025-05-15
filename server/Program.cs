76302103f822eba6cbc0127ddc66ce6402b5dd41
George <utereskovygmail.com@MacBook-Pro-Yurii.local>
2025-05-15 17:08:41 +0300

    Initial commit

---
+++ server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var endpoint = new IPEndPoint(IPAddress.Any, 9999);
+server.Bind(endpoint);
+
+server.Listen(10);
+
+Console.WriteLine("Server started");
+
+Socket client = server.Accept();
+
+Console.WriteLine("Client connected");
+
+var buffer = new byte[1024];
+int sizeBytes = client.Receive(buffer);
+
+
+var text = Encoding.ASCII.GetString(buffer, 0, sizeBytes);
+
+Console.WriteLine("Received" + text);
+
+string send = "Thanks buddy";
+client.Send(Encoding.ASCII.GetBytes(send));
+
+Console.WriteLine("Sent " + send);
+
+client.Shutdown(SocketShutdown.Both);
+client.Close();
---
+++ сlient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999));
+
+string message = "Hello Nigger";
+
+clientSocket.Send(Encoding.ASCII.GetBytes(message));
+
+byte[] buffer = new byte[1024];
+clientSocket.Receive(buffer);
+string response = Encoding.ASCII.GetString(buffer);
+
+Console.WriteLine("response - "+response);
+
+clientSocket.Shutdown(SocketShutdown.Both);
+clientSocket.Close();
+
